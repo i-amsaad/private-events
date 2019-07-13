e2bd9a858ef590542129d539c93af30891edb6d4
Saad <muhammadsaad7891@gmail.com>
2019-07-13 12:49:54 -0700

    Create events controller and routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
 	root to: 'sessions#new'
 	resources:users
 	resources:sessions
+	resources:events
 	get '/signin', to: 'sessions#new'
+	get '/signout', to: 'sessions#destroy'
 end
