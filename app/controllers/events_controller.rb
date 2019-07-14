7d0515199491fa47a6e0e854a2e4ee501c4e69c5
Saad <muhammadsaad7891@gmail.com>
2019-07-14 12:22:04 -0700

    Put Navigations to jump around

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,5 @@
 class EventsController < ApplicationController
+	before_action :check_loginned, only: [:new, :create]
 	def index
 		@upcoming=Event.upcoming_events
 		@previous=Event.previous_events
@@ -15,8 +16,21 @@ class EventsController < ApplicationController
 	def show
 		@event=Event.find(params[:id])
 		@attendees=@event.attendees
+		@current_user_attendent=false
+		if session[:id]!=nil
+			@current_user_attendent=@attendees.exists?(session[:id])
+		end
+
 	end
+	
 	def event_params
 		params.require(:event).permit(:date,:description)
+	end
+
+	def check_loginned
+		if session[:id]==nil
+			flash[:notice]="Please, Login to create an Event"
+			redirect_to("/signin")
+		end
 	end
 end
