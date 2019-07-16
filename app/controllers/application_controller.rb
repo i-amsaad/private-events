8a54ba60478bddd58627fb25ae8f1bb6fe519c38
Saad <muhammadsaad7891@gmail.com>
2019-07-16 02:27:52 -0700

    Add member function to routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,9 @@
 class ApplicationController < ActionController::Base
   before_action :set_user
   def set_user
-    if !session[:id].blank?
-      @current_user=User.find(session[:id])
-    end
+    @current_user = User.find_by(id: session[:id])
   end
+
   def check_loggedin
     if !@current_user.blank?
       flash[:notice]="You are already logged in."
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,12 @@ Rails.application.routes.draw do
   root to: 'events#index'
   resources:users
   resources:sessions
-  resources:events
+  resources:events do
+    member do
+      post :attending
+    end
+  end
   get '/signin', to: 'sessions#new'
   get '/signup', to: 'users#new'
   get '/signout', to: 'sessions#destroy'
-  post 'attending' => 'events#attending'
 end
